4a413b293d2d31b78c6c1e9f3f4d3baf6c85d209
EyuAtske <EyuelA.Fekade21@gmail.com>
2026-05-21 10:15:54 +0300

    create a connection with rabbitmq and closes it when a ctl + c signal is sent

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	con, err := amqp.Dial(connStr)
+	if err != nil{
+		fmt.Println("Connection faild: %w", err)
+	}
+	defer con.Close()
+	fmt.Println("Connection was successfull")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("program is shutting down")
+	con.Close()
 }
