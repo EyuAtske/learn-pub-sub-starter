f23268fe7b3aca3c86c8f5149e485739169fb265
EyuAtske <EyuelA.Fekade21@gmail.com>
2026-05-27 20:10:50 +0300

    used stomp for rabbitmq and added exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -15,6 +17,12 @@ func main() {
 		fmt.Println("Connection faild: %w", err)
 	}
 	defer con.Close()
+	ch, err := con.Channel()
+	if err != nil{
+		fmt.Print("coundn't create channel")
+		return
+	}
+	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
 	fmt.Println("Connection was successfull")
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
